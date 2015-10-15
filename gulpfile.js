cd6bcb69f1b9972f3f42cec75ea669dbe02653fa
makotot <makoto.ttn@gmail.com>
2015-10-15 14:03:02 +0900

    update

--- gulpfile.js
+++ gulpfile.js
@@ -47,7 +47,81 @@ gulp.task('css', function () {
           "property-whitelist": [2, ["display"]],// 許可するプロパティをリストで指定すること / option: array
 
           // Custom property
-          "custom-property-no-outside-root": [2]// :rootセレクタ以外でのカスタムプロパティを許可しないこと
+          "custom-property-no-outside-root": [2],// :rootセレクタ以外でのカスタムプロパティを許可しないこと
+          "custom-property-pattern": [2, /foo-.+/],// カスタムプロパティのパターンを指定すること / option: regex | string
+
+          // Declaration
+          "declaration-bang-space-after": [2, "never"],// エクスクラメーションマークの直後にスペースを入れることを許可するか否かを統一すること / option: "always" | "never"
+          "declaration-bang-space-before": [2, "always"],// エクスクラメーションマークの直前にスペースを入れることを許可するか否かを統一すること / option: "always" | "never"
+          "declaration-colon-newline-after": [2, "always-multi-line"],// コロンの直後に改行を必須にするかスペースを許可しないか統一すること / option: "always" | "always-multi-line"
+          "declaration-colon-space-after": [2, "always"],// コロンの直後にスペースを入れることを許可するか否かを統一すること / option: "always" | "never" | "always-single-line"
+          "declaration-colon-space-before": [2, "never"],// コロンの直前にスペースを入れることを許可するか否かを統一すること / option: "always" | "never"
+          "declaration-no-important": [2],// !important の使用を許可しないこと
+
+          // Declaration block
+          "declaration-block-semicolon-newline-after": [2, "always-multi-line"],// セミコロンの直後にスペースを許可しないか改行を必須にするかを統一すること / option: "always" | "always-multi-line" | "never-multi-line"
+          "declaration-block-semicolon-newline-before": [2, "never-multi-line"],// セミコロンの直前にスペースを許可しないか改行を必須にするかを統一すること / option: "always" | "always-multi-line" | "never-multi-line"
+          "declaration-block-semicolon-space-after": [2, "always"],// セミコロンの直後にスペースを入れることを許可するか否かを統一すること / option: "always" | "never" | "always-single-line" | "never-single-line"
+          "declaration-block-semicolon-space-before": [2, "never"],// セミコロンの直前にスペースを入れることを許可するか否かを統一すること / option: "always" | "never" | "always-single-line" | "never-single-line"
+
+          // Nesting Block
+          "nesting-block-opening-brace-newline-before": [2, "always-multi-line"],// ネストしたブロックの開き括弧の直前に改行を必須にするかスペースを許可しないかと統一すること / option: "always" | "always-single-line" | "never-single-line" | "always-multi-line" | "never-multi-line"
+          "nesting-block-opening-brace-space-before": [2, "never"],// ネストしたブロックの開き括弧の直前にスペースを許可するか否かを統一すること / option: "always" | "never" | "always-single-line" | "never-single-line" | "always-multi-line" | "never-multi-line"
+
+          // Block
+          "block-closing-brace-newline-after": [2, "always"],// ブロックの閉じ括弧の直後に改行を必須にするかスペースを許可しないかを統一すること / option: "always" | "always-single-line" | "never-single-line" | "always-multi-line" | "never-multi-line"
+          "block-closing-brace-newline-before": [2, "always"],// ブロックの閉じ括弧の直前に改行を必須にするかスペースを許可しないかを統一すること / option: "always" | "always-multi-line" | "never-multi-line"
+          "block-closing-brace-space-after": [2, "always"],// ブロックの閉じ括弧の直後にスペースを許可するか否かを統一すること / option: "always" | "never" | "always-single-line" | "never-single-line" | "always-multi-line" | "never-multi-line"
+          "block-closing-brace-space-before": [2, "never"],// ブロックの閉じ括弧の直前にスペースを許可するか否かを統一すること / option: "always" | "never" | "always-single-line" | "never-single-line" | "always-multi-line" | "never-multi-line"
+          "block-no-empty": [2],// 空のブロックを許可しないこと
+          "block-opening-brace-newline-after": [2, "always"],// ブロックの開き括弧の直後に改行を必須にすること / option: "always" | "always-multi-line" | "never-multi-line"
+          "block-opening-brace-newline-before": [2, "always-multi-line"],// ブロックの開き括弧の直前に改行を必須にするかスペースを許可しないかを統一すること / option: "always" | "always-single-line"|"never-single-line"|"always-multi-line"|"never-multi-line"
+          "block-opening-brace-space-after": [2, "never-single-line"],// ブロックの開き括弧の直後に改行を必須にするかスペースを許可しないかを統一すること / option: "always"|"never"|"always-single-line"|"never-single-line"|"always-multi-line"|"never-multi-line"
+          "block-opening-brace-space-before": [2, "always"],// ブロックの開き括弧の直前にスペースを許可するか否かを統一すること / option: "always"|"never"|"always-single-line"|"never-single-line"|"always-multi-line"|"never-multi-line"
+          // Selector
+          "selector-combinator-space-after": [2, "always"],// セレクタの組み合わせの直後にスペースを許可するか否かを統一すること / option: "always" | "never"
+          "selector-combinator-space-before": [2, "always"],// セレクタの組み合わせの直前にスペースを許可するか否かを統一すること / option: "always" | "never"
+          "selector-no-attribute": [2],// 属性セレクタを許可しないこと
+          "selector-no-combinator": [2],// セレクタの組み合わせを許可しないこと
+          "selector-no-id": [2],// idセレクタを許可しないこと
+          "selector-no-type": [2],// 要素型セレクタを許可しないこと
+          "selector-no-universal": [2],// ユニバーサルセレクタを許可しないこと
+          "selector-no-vendor-prefix": [2],// ベンダープレフィックスつきのセレクタを許可しないこと
+          "selector-pseudo-element-colon-notation": [2, "double"],// 疑似要素のコロン表記を1つか2つか統一すること / option: "single" | "double"
+          "selector-root-no-composition": [2],// :root を他のセレクタと合わせて記述することを許可しないこと
+
+          // Selector list
+          "selector-list-comma-newline-after": [2, "always-multi-line"],// セレクタのリストで、カンマの直後に改行を必須にするかスペースを許可しないかを統一すること / option: "always"|"always-multi-line"|"never-multi-line"
+          "selector-list-comma-newline-before": [2, "never-multi-line"],// セレクタのリストで、カンマの直前に改行を必須にするかスペースを許可しないかを統一すること / option: "always"|"always-multi-line"|"never-multi-line"
+          "selector-list-comma-space-after": [2, "always-single-line"],// セレクタのリストで、カンマの直後にスペースを許可するか否かを統一すること / option: "always"|"never"|"always-single-line"|"never-single-line"
+          "selector-list-comma-space-before": [2, "never"],// セレクタのリストで、カンマの直前にスペースを許可するか否かを統一すること / option: "always"|"never"|"always-single-line"|"never-single-line"
+
+          // Rule
+          "rule-nested-empty-line-before": [2, "always"],// ネストしたルールの直前に空白行を必須にするか許可しないかを統一すること / option: "always"|"never"|"always-multi-line"|"never-multi-line"
+          "rule-no-duplicate-properties": [2],// ルールの中でプロパティの重複を許可しないこと
+          "rule-no-shorthand-property-overrides": [2],// ショートハンドプロパティで上書きすることを許可しないこと
+          "rule-no-single-line": [2],// 単一行でルールを記述することを許可しないこと
+          "rule-non-nested-empty-line-before": [2, "always"],// ネストしてないルールの直前に空白行を必須にするか許可しないかを統一すること / option: "always"|"never"|"always-multi-line"|"never-multi-line"
+          "rule-properties-order": [2, "alphabetical"],// ルールの中でプロパティの順序を指定すること / option: "alphabetical" | array
+          "rule-trailing-semicolon": [2, "always"],// 省略可能なセミコロンを必須にするか許可しないかを統一すること / option: "always" | "never"
+
+          // Root
+          "root-no-standard-properties": [2],// 標準のプロパティを :root の中で許可しないこと
+
+          // Media feature
+          "media-feature-colon-space-after": [2, "always"],// メディアフィーチャーの中で、コロンの直後にスペースを必須にするか許可しないかを統一すること / option: "always" | "never"
+          "media-feature-colon-space-before": [2, "never"],// メディアフィーチャーの中で、コロンの直前にスペースを必須にするか許可しないかを統一すること / option: "always" | "never"
+          "media-feature-name-no-vendor-prefix": [2],// メディアフィーチャーの名前にベンダープレフィックスを許可しないこと
+          "media-feature-range-operator-space-after": [2, "always"],// メディアフィーチャーの中で、範囲演算子の直後にスペースを必須にするか許可しないかを統一すること / option: "always" | "never"
+          "media-feature-range-operator-space-before": [2, "always"],// メディアフィーチャーの中で、範囲演算子の直前にスペースを必須にするか許可しないかを統一すること / option: "always" | "never"
+
+          // Custom media
+          "custom-media-pattern": [2, /foo-.+/],// カスタムメディアクエリの命名パターンを指定すること / option: regex | string
+
+          // Media query
+          "media-query-parentheses-space-inside": [2, "never"],// メディアクエリの括弧の内側にスペースを必須にするか許可しないかを統一すること / option: "always" | "never"
+          // Media query list
+          "media-query-list-comma-newline-after": [2, "never-multi-line"]// メディアクエリのリストで、カンマの直後に改行を必須にするかスペースを許可しないか統一すること / option: "always"|"always-multi-line"|"never-multi-line"
         }
       }),
       require('postcss-reporter')({
