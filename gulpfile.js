cdd8eaf819c3bd4cbb5433931e08d47f7f1b77ee
makotot <makoto.ttn@gmail.com>
2015-10-17 10:53:14 +0900

    update stylelint

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,12 @@
 var gulp = require('gulp'),
   postcss = require('gulp-postcss');
 
-var rc = require('./.stylelintrc.json');
-
 gulp.task('css', function () {
   return gulp
     .src('./src/css/*.css')
     .pipe(postcss([
       require('autoprefixer')(),
-      require('stylelint')(rc),
+      require('stylelint')(),
       require('postcss-reporter')({
         clearMessages: true
       })//,
